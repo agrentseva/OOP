c3fd8310353f646cdc9d26b78b7b6244dc23227b
agrentseva <a.grentseva@g.nsu.ru>
2025-09-12 23:00:09 +0700

    shell-script

---
+++ Task_1_1_1/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p my_build documentation
+
+#compile
+javac -d my_build src/main/java/ru/nsu/ga/grentseva/sort/Sort.java
+javac -d my_build -cp build src/test/java/ru/nsu/ga/grentseva/sort/SortTest.java
+
+#documentation
+javadoc -d documentation src/main/java/ru/nsu/ga/grentseva/sort/Sort.java src/test/java/ru/nsu/ga/grentseva/sort/SortTest.java
+
+#JAR-file
+jar cfe app.jar ru.nsu.ga.grentseva.sort.Sort -C my_build .
+
+#start
+java -jar app.jar
