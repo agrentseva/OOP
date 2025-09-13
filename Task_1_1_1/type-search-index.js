4055eddec6a653a4ff70f397f702c0c8eb9b14f3
agrentseva <agrentseva@users.noreply.github.com>
2025-09-13 15:53:11 +0000

    Deploying to gh-pages from @ agrentseva/OOP@2b08180e0636d2122a2715a9813c6603e2483445 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.ga.grentseva.sort","l":"Sort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.ga.grentseva.sort","l":"Main"},{"p":"ru.nsu.ga.grentseva.sort","l":"Sort"}];updateSearchResults();
