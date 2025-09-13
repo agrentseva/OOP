123b6910cfce34bf09dfd1284673f66e790c11c5
agrentseva <a.grentseva@g.nsu.ru>
2025-09-13 13:08:45 +0700

    javadoc

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -7,7 +7,7 @@ javac -d my_build src/main/java/ru/nsu/ga/grentseva/sort/Sort.java
 javac -d my_build -cp build src/test/java/ru/nsu/ga/grentseva/sort/SortTest.java
 
 #documentation
-javadoc -d documentation src/main/java/ru/nsu/ga/grentseva/sort/Sort.java src/test/java/ru/nsu/ga/grentseva/sort/SortTest.java
+javadoc -d documentation -sourcepath src/main/java ru.nsu.ga.grentseva.sort
 
 #JAR-file
 jar cfe app.jar ru.nsu.ga.grentseva.sort.Sort -C my_build .
