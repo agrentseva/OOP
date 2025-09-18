a776a6898d57b708765b40c46e4d01e91b5357a9
agrentseva <a.grentseva@g.nsu.ru>
2025-09-18 18:04:14 +0700

    change name, delete comments, change order of func, change return, add private cons and checking null

--- Task_1_1_1/src/main/java/ru/nsu/ga/grentseva/sort/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/ga/grentseva/sort/Main.java
@@ -2,16 +2,10 @@ package ru.nsu.ga.grentseva.sort;
 
 import java.util.Arrays;
 
-/**
- * Class for starting program.
- */
 public class Main {
-    /**
-     * Check simple test.
-     */
     public static void main(String[] args) {
         int[] array = {5, 8, 5, 8, 5, 8};
-        int[] sortArray = Sort.heap_sort(array);
-        System.out.println("After sorting:  " + Arrays.toString(array));
+        int[] sortArray = HeapSort.heap_sort(array);
+        System.out.println("After sorting:  " + Arrays.toString(sortArray));
     }
 }
