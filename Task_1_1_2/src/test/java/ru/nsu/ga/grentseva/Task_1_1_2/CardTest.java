3911ae320812d3a10c98b53dc2c50cd51be59134
agrentseva <a.grentseva@g.nsu.ru>
2025-09-19 23:45:08 +0700

    aaaaaaaaaa

---
+++ Task_1_1_2/src/test/java/ru/nsu/ga/grentseva/Task_1_1_2/CardTest.java
@@ -0,0 +1,23 @@
+package ru.nsu.ga.grentseva.Task_1_1_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void testCardValues() {
+        Card aceOfSpades = new Card(CardRank.ACE, CardSuit.SPADES);
+        assertEquals(11, aceOfSpades.getCardValue());
+
+        Card kingOfHearts = new Card(CardRank.KING, CardSuit.HEARTS);
+        assertEquals(10, kingOfHearts.getCardValue());
+    }
+
+    @Test
+    void testCardName() {
+        Card queenOfDiamonds = new Card(CardRank.QUEEN, CardSuit.DIAMONDS);
+        assertTrue(queenOfDiamonds.getCardName().contains("дама"));
+    }
+}
