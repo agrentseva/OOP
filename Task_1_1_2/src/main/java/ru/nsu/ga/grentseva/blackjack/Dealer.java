07178337e4b8ba2645b05fb2592c4556a1854c8f
agrentseva <a.grentseva@g.nsu.ru>
2025-09-20 11:14:24 +0700

    test + style

--- Task_1_1_2/src/main/java/ru/nsu/ga/grentseva/blackjack/Dealer.java
+++ Task_1_1_2/src/main/java/ru/nsu/ga/grentseva/blackjack/Dealer.java
@@ -1,14 +1,12 @@
 package ru.nsu.ga.grentseva.blackjack;
 
-class Dealer extends Player {
-
+public class Dealer extends Player {
     public void dealerPlay(Deck deck) {
         while (getHandValue() < 17) {
             Card newCard = deck.take();
             addCard(newCard);
 
             System.out.println("Дилер открывает карту: " + newCard);
-
         }
     }
 }
--- Task_1_1_2/src/test/java/ru/nsu/ga/grentseva/blackjack/PlayerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ga/grentseva/blackjack/PlayerTest.java
@@ -1,26 +1,66 @@
 package ru.nsu.ga.grentseva.blackjack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 
 
 class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
 
     @Test
     void testAddCardAndHandValue() {
-        Player player = new Player();
         player.addCard(new Card(CardRank.FIVE, CardSuit.HEARTS));
         player.addCard(new Card(CardRank.SIX, CardSuit.CLUBS));
         assertEquals(11, player.getHandValue());
     }
 
     @Test
+    void testGetHandValue_EmptyHand() {
+        assertEquals(0, player.getHandValue());
+    }
+
+    @Test
+    void testIsBust_WhenBelow21() {
+        player.addCard(new Card(CardRank.TEN, CardSuit.HEARTS));
+        player.addCard(new Card(CardRank.FIVE, CardSuit.SPADES));
+        assertFalse(player.isBust());
+    }
+
+    @Test
+    void testIsBust_WhenOver21() {
+        player.addCard(new Card(CardRank.TEN, CardSuit.HEARTS));
+        player.addCard(new Card(CardRank.TEN, CardSuit.SPADES));
+        player.addCard(new Card(CardRank.TWO, CardSuit.HEARTS));
+
+        assertTrue(player.isBust());
+    }
+
+    @Test
     void testClearHand() {
-        Player player = new Player();
         player.addCard(new Card(CardRank.TEN, CardSuit.DIAMONDS));
         player.clearHand();
         assertEquals(0, player.getHandCards().size());
+    }
+
+    @Test
+    void testHasBlackjack() {
+        player.addCard(new Card(CardRank.ACE, CardSuit.HEARTS));
+        player.addCard(new Card(CardRank.TEN, CardSuit.HEARTS));
+
+        assertTrue(player.hasBlackjack());
+
+        player.addCard(new Card(CardRank.TEN, CardSuit.CLUBS));
+        assertFalse(player.hasBlackjack());
+
     }
 }
