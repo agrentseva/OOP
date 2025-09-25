391cc212953d2e7b021b650b78e38e03bcebb757
agrentseva <a.grentseva@g.nsu.ru>
2025-09-25 13:05:07 +0700

    update output test

--- Task_1_1_2/src/test/java/ru/nsu/ga/grentseva/blackjack/ConsoleOutputTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ga/grentseva/blackjack/ConsoleOutputTest.java
@@ -1,5 +1,4 @@
 package ru.nsu.ga.grentseva.blackjack;
-
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -10,37 +9,59 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ConsoleOutputTest {
 
     @Test
-    void testConsoleOutput() {
-        // Перехватываем System.out
-        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(testOut));
-
+    void testAllConsoleOutputMethods() {
+        ByteArrayOutputStream testingOut = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(testingOut));
+        
         ConsoleOutput output = new ConsoleOutput();
         Player player = new Player();
         Dealer dealer = new Dealer(output);
         Deck deck = new Deck(1);
         deck.shuffle();
-
+        
         player.addCard(deck.take());
         player.addCard(deck.take());
         dealer.addCard(deck.take());
         dealer.addCard(deck.take());
         Card card = deck.take();
-
+        
         output.printWelcome();
         output.printRoundStart(1);
+        output.showInitialCards(player, dealer);
+        output.printPlayerBlackjack();
         output.printPlayerTurn();
         output.printPlayerCard(card, player, dealer);
+        output.printPlayerBust();
+        output.printDealerTurn(dealer, player);
+        output.printDealerCard(card, dealer, player);
+        output.printDealerBust();
+        output.printPlayerWin();
+        output.printDealerWin();
+        output.printDraw();
         output.printScore(1, 0);
-
-        String text = testOut.toString();
-        assertTrue(text.contains("Добро пожаловать в Блэкджек!"));
-        assertTrue(text.contains("\nРаунд 1"));
-        assertTrue(text.contains("\nВаш ход\n-------"));
+        output.printInvalidInput();
+        
+        String text = testingOut.toString();
+        
+        assertTrue(text.contains("Добро пожаловать"));
+        assertTrue(text.contains("Раунд 1"));
+        assertTrue(text.contains("Дилер раздал карты"));
+        assertTrue(text.contains("Блэкджек!"));
+        assertTrue(text.contains("Ваш ход"));
         assertTrue(text.contains("Вы открыли карту"));
+        assertTrue(text.contains("Перебор"));
+        assertTrue(text.contains("Ход дилера"));
+        assertTrue(text.contains("Дилер открывает карту"));
+        assertTrue(text.contains("Дилер перебрал"));
+        assertTrue(text.contains("Вы выиграли раунд"));
+        assertTrue(text.contains("Вы проиграли раунд"));
+        assertTrue(text.contains("Ничья"));
         assertTrue(text.contains("Счёт: 1:0"));
-
-        System.setOut(System.out);
+        assertTrue(text.contains("Неверный ввод"));
+        
+        System.setOut(originalOut);
     }
 }
+
 
