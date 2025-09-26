c61131a2540100b00454cb0c4af1f91f180c6e65
agrentseva <a.grentseva@g.nsu.ru>
2025-09-26 18:03:07 +0700

    mini strange test for all coverage (╥_╥)

---
+++ Task_1_1_2/src/test/java/ru/nsu/ga/grentseva/blackjack/RoundTest.java
@@ -0,0 +1,34 @@
+package ru.nsu.ga.grentseva.blackjack;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+class RoundTest {
+
+    private ConsoleOutput output = new ConsoleOutput();
+    private Random random = new Random();
+    private int roundCounter = 0;
+    private Player player = new Player();
+    private Dealer dealer = new Dealer(output);
+
+    private ConsoleInput input = new ConsoleInput() {
+        @Override
+        public int askPlayerChoice() {
+            return 0;
+        }
+    };
+
+    @Test
+    void testResult() {
+        Round round = new Round(roundCounter, player, dealer, input, output, random);
+        Round.Result result = round.play();
+
+        assertTrue(result == Round.Result.PLAYER_WIN
+                || result == Round.Result.DEALER_WIN
+                || result == Round.Result.DRAW
+        );
+    }
+}
+
