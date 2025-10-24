cbe635ac9f035927ec1a25bd96875345b65829e1
agrentseva <a.grentseva@g.nsu.ru>
2025-10-24 21:13:20 +0700

    tests

--- Task_1_2_1/src/main/java/ru/nsu/ga/grentseva/graph/GraphSort/TopologicalSortDFS.java
+++ Task_1_2_1/src/main/java/ru/nsu/ga/grentseva/graph/GraphSort/TopologicalSortDFS.java
@@ -4,44 +4,44 @@ import ru.nsu.ga.grentseva.graph.GeneralGraphClasses.Graph;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
-public class GraphSort {
+public class TopologicalSortDFS implements GraphSorter {
 
-    private final Graph graph;
-    int n;
-    private final boolean [] visited;
-    private final boolean [] inStack;
-    ArrayList<Integer> sortArray;
+    private boolean[] visited;
+    private boolean[] inStack;
+    private ArrayList<Integer> sortArray;
 
-    public GraphSort (Graph graph) {
-        this.graph = graph;
-        n = graph.getVertexCount();
+    @Override
+    public List<Integer> sort(Graph graph) {
+        int n = graph.getVertexCount();
+
         visited = new boolean[n + 1];
         inStack = new boolean[n + 1];
         sortArray = new ArrayList<>();
-    }
 
-    public ArrayList<Integer> topologicalSort() {
-        for(int vertex = 1; vertex <= n; vertex++) {
+        for (int vertex = 1; vertex <= n; vertex++) {
             if (!visited[vertex]) {
-                dfs(vertex);
+                dfs(graph, vertex);
             }
         }
+
         Collections.reverse(sortArray);
         return sortArray;
     }
 
-    private void dfs(int v) {
+    private void dfs(Graph graph, int v) {
         visited[v] = true;
         inStack[v] = true;
 
         for (int neighbor : graph.getNeighbors(v)) {
-            if (!visited[neighbor]){
-                dfs(neighbor);
-            } else if (inStack[neighbor]){
+            if (!visited[neighbor]) {
+                dfs(graph, neighbor);
+            } else if (inStack[neighbor]) {
                 throw new RuntimeException("Error: the graph contains a cycle.");
             }
         }
+
         inStack[v] = false;
         sortArray.add(v);
     }
@@ -49,13 +49,9 @@ public class GraphSort {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(sortArray.get(i));
-            if (i < n - 1) {
-                sb.append(" ");
-            }
+        for (int v : sortArray) {
+            sb.append(v).append(" ");
         }
-        return sb.toString();
+        return sb.toString().trim();
     }
-
 }
--- Task_1_2_1/src/main/java/ru/nsu/ga/grentseva/graph/Main.java
+++ Task_1_2_1/src/main/java/ru/nsu/ga/grentseva/graph/Main.java
@@ -1,61 +1,57 @@
 package ru.nsu.ga.grentseva.graph;
 
-import ru.nsu.ga.grentseva.graph.GraphSort.GraphSort;
+import ru.nsu.ga.grentseva.graph.GraphSort.TopologicalSortDFS;
 import ru.nsu.ga.grentseva.graph.ReadGraphFromFile.ReadGraph;
 import ru.nsu.ga.grentseva.graph.RepresentationsOfGraph.AdjacencyListGraph;
 import ru.nsu.ga.grentseva.graph.RepresentationsOfGraph.AdjacencyMatrixGraph;
 import ru.nsu.ga.grentseva.graph.RepresentationsOfGraph.IncidenceMatrixGraph;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
+
     public static void main(String[] args) {
-        ReadGraph dataGraph = ReadGraph.readGraphFromFile("C:\\Users\\emoch\\OOP\\Task_1_2_1\\src\\main\\java\\ru\\nsu\\ga\\grentseva\\graph\\test");
-        
+
+        ReadGraph dataGraph = ReadGraph.readGraphFromFile(
+                "C:\\Users\\emoch\\OOP\\Task_1_2_1\\src\\main\\java\\ru\\nsu\\ga\\grentseva\\graph\\test"
+        );
+
         AdjacencyMatrixGraph graph1 = new AdjacencyMatrixGraph(dataGraph);
         System.out.println(graph1);
+
         AdjacencyMatrixGraph graph2 = new AdjacencyMatrixGraph(dataGraph);
-        if (graph1.equals(graph2)){
+        if (graph1.equals(graph2)) {
             System.out.println("Super");
         }
         System.out.println();
 
-        GraphSort sorter1 = new GraphSort(graph1);
-        ArrayList<Integer> res1 = sorter1.topologicalSort();
-        for (int vertex : res1) {
-            System.out.print(vertex + " ");
-        }
+        TopologicalSortDFS sorter = new TopologicalSortDFS();
+        sorter.sort(graph1);
+        System.out.println("Topological DFS (AdjacencyMatrix): " + sorter);
         System.out.println();
+
 
         IncidenceMatrixGraph graph3 = new IncidenceMatrixGraph(dataGraph);
         System.out.println(graph3);
-        if (graph1.equals(graph3)){
+
+        if (graph1.equals(graph3)) {
             System.out.println("Super");
         }
         System.out.println();
 
-        GraphSort sorter2 = new GraphSort(graph3);
-        ArrayList<Integer> res2 = sorter2.topologicalSort();
-        for (int vertex : res2) {
-            System.out.print(vertex + " ");
-        }
+        sorter.sort(graph3);
+        System.out.println("Topological DFS (IncidenceMatrix): " + sorter);
         System.out.println();
+
 
         AdjacencyListGraph graph4 = new AdjacencyListGraph(dataGraph);
         System.out.println(graph4);
-        if (graph1.equals(graph4)){
+
+        if (graph1.equals(graph4)) {
             System.out.println("Super");
         }
         System.out.println();
 
-        GraphSort sorter3 = new GraphSort(graph4);
-        ArrayList<Integer> res3 = sorter3.topologicalSort();
-        for (int vertex : res3) {
-            System.out.print(vertex + " ");
-        }
+        sorter.sort(graph4);
+        System.out.println("Topological DFS (AdjacencyList): " + sorter);
         System.out.println();
-
     }
 }
