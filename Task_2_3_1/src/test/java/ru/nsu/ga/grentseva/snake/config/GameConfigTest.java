e5899a580c483c7f6d3f043ad23a6c6ab36eaaf2
agrentseva <a.grentseva@g.nsu.ru>
2026-04-10 16:03:28 +0700

    changes

--- Task_2_3_1/src/test/java/ru/nsu/ga/grentseva/snake/config/GameConfigTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/ga/grentseva/snake/config/GameConfigTest.java
@@ -9,8 +9,8 @@ public class GameConfigTest {
     public void testLevel1Config() {
         GameConfig config = GameConfig.level1();
         assertNotNull(config);
-        assertEquals(35, config.width);
-        assertEquals(25, config.height);
+        assertEquals(20, config.width);
+        assertEquals(30, config.height);
         assertEquals(5, config.targetLength);
         assertEquals(3, config.foodCount);
         assertEquals(1.0, config.speed, 0.001);
@@ -22,8 +22,8 @@ public class GameConfigTest {
     public void testLevel2Config() {
         GameConfig config = GameConfig.level2();
         assertNotNull(config);
-        assertEquals(35, config.width);
-        assertEquals(25, config.height);
+        assertEquals(20, config.width);
+        assertEquals(30, config.height);
         assertEquals(7, config.targetLength);
         assertEquals(5, config.foodCount);
         assertEquals(1.5, config.speed, 0.001);
@@ -35,8 +35,8 @@ public class GameConfigTest {
     public void testEndlessConfig() {
         GameConfig config = GameConfig.endless();
         assertNotNull(config);
-        assertEquals(35, config.width);
-        assertEquals(25, config.height);
+        assertEquals(100, config.width);
+        assertEquals(100, config.height);
         assertEquals(Integer.MAX_VALUE, config.targetLength);
         assertEquals(5, config.foodCount);
         assertEquals(1.0, config.speed, 0.001);
