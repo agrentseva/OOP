69704cd52492b3406fa78d68117cef7f243aaa30
agrentseva <a.grentseva@g.nsu.ru>
2026-05-29 01:45:55 +0700

    fix test

--- Task_2_1_2/src/test/java/ru/nsu/ga/grentseva/primenumbers/worker/WorkerHandlerTest.java
+++ Task_2_1_2/src/test/java/ru/nsu/ga/grentseva/primenumbers/worker/WorkerHandlerTest.java
@@ -1,16 +1,19 @@
 package ru.nsu.ga.grentseva.primenumbers.worker;
 
 import org.junit.jupiter.api.Test;
+import ru.nsu.ga.grentseva.primenumbers.common.MessageType;
 import ru.nsu.ga.grentseva.primenumbers.common.Task;
 import ru.nsu.ga.grentseva.primenumbers.common.TaskResult;
 import ru.nsu.ga.grentseva.primenumbers.common.TaskStatus;
+import ru.nsu.ga.grentseva.primenumbers.common.WorkerMessage;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WorkerHandlerTest {
 
@@ -28,22 +31,38 @@ class WorkerHandlerTest {
         });
         serverThread.start();
 
-        Socket clientSocket = new Socket("localhost", 6000);
-        ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-        ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
+        try (
+                Socket clientSocket = new Socket("localhost", 6000);
+                ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream())
+        ) {
+            Task task = new Task(1, new int[]{2, 3, 4});
+            outputStream.writeObject(task);
+            outputStream.flush();
 
-        Task task = new Task(1, new int[]{2, 3, 4});
-        outputStream.writeObject(task);
-        outputStream.flush();
+            boolean resultReceived = false;
 
-        Object response = inputStream.readObject();
-        assertTrue(response instanceof TaskResult);
+            while (!resultReceived) {
+                Object response = inputStream.readObject();
+                assertTrue(response instanceof WorkerMessage);
 
-        TaskResult result = (TaskResult) response;
-        assertEquals(TaskStatus.COMPLETED, result.getStatus());
-        assertTrue(result.hasNonPrime());
+                WorkerMessage message = (WorkerMessage) response;
 
-        clientSocket.close();
+                if (message.getType() == MessageType.HEARTBEAT) {
+                    continue;
+                }
+
+                if (message.getType() == MessageType.RESULT) {
+                    TaskResult result = message.getResult();
+
+                    assertEquals(TaskStatus.COMPLETED, result.getStatus());
+                    assertTrue(result.hasNonPrime());
+
+                    resultReceived = true;
+                }
+            }
+        }
+
         serverSocket.close();
     }
 }
